72c6f6a53626c9a17eedc074e24115a57b8bc065
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-03-23 20:47:58 +0100

    Refactor documentation

--- src/main/java/org/example/Router/SimpleRouteBuilder.java
+++ src/main/java/org/example/Router/SimpleRouteBuilder.java
@@ -7,10 +7,10 @@ public class SimpleRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        /** After copy files from inputFolder to output, will erase files from inputFolder */
+        /** After copy files from inputFolder to output, will move files from inputFolder to inputFolder/.camel */
         from("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/inputFolder").to("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/outputFolder");
 
-        /** After copy files from inputFolder to output, will NOT erase files from inputFolder */
+        /** After copy files from inputFolder to output, will leave files in inputFolder */
 //        from("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/inputFolder?noop=true").to("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/outputFolder");
     }
 }
