6d5474844caa56445b741820b19879a9e2f0d3dd
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-03-23 20:44:35 +0100

    Add option with erasing files from inputFolder after copying

--- src/main/java/org/example/Router/SimpleRouteBuilder.java
+++ src/main/java/org/example/Router/SimpleRouteBuilder.java
@@ -6,9 +6,11 @@ public class SimpleRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/inputFolder?noop=true").to("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/outputFolder");
 
+        /** After copy files from inputFolder to output, will erase files from inputFolder */
+        from("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/inputFolder").to("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/outputFolder");
 
-
+        /** After copy files from inputFolder to output, will NOT erase files from inputFolder */
+//        from("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/inputFolder?noop=true").to("file:/home/acer/PROGRAMMING/Jobs/PKO BP JJD/outputFolder");
     }
 }
